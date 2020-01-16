bfde13c9322c1651d1f81e5571ad27d1a2ce404e
Mahmoud Jafarinejad <mahmoud.jafarinejad@klarna.com>
2020-01-16 16:18:42 +0100

    setup project and dependencies

--- app/src/main/java/com/klarna/sample/hybrid/SampleActivity.kt
+++ app/src/main/java/com/klarna/sample/hybrid/SampleActivity.kt
@@ -3,10 +3,10 @@ package com.klarna.sample.hybrid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sample)
     }
 }
