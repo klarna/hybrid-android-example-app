9a9f88fa373a6a8a7f8f4f582a1a1035a75bf81a
Mahmoud Jafarinejad <mahmoud.jafarinejad@klarna.com>
2020-01-16 17:01:10 +0100

    added web view activity and connected it to the main activity

--- app/src/main/java/com/klarna/sample/hybrid/MainActivity.kt
+++ app/src/main/java/com/klarna/sample/hybrid/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.klarna.sample.hybrid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        buttonKlarnaDemo.setOnClickListener {
+            startSDK("https://www.klarna.com/demo/")
+        }
+
+        buttonCustomUrl.setOnClickListener {
+            startSDK(null)
+        }
+    }
+
+    private fun startSDK(url: String?) {
+        Intent(this, WebViewActivity::class.java)
+            .apply {
+                putExtra(WebViewActivity.KEY_URL, url)
+                startActivity(this)
+            }
     }
 }
---
+++ app/src/main/java/com/klarna/sample/hybrid/WebViewActivity.kt
@@ -0,0 +1,138 @@
+package com.klarna.sample.hybrid
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.klarna.mobile.sdk.KlarnaMobileSDKError
+import com.klarna.mobile.sdk.api.OnCompletion
+import com.klarna.mobile.sdk.api.hybrid.KlarnaHybridSDK
+import com.klarna.mobile.sdk.api.hybrid.KlarnaHybridSDKCallback
+import kotlinx.android.synthetic.main.activity_webview.*
+
+class WebViewActivity : AppCompatActivity(), KlarnaHybridSDKCallback {
+
+    companion object {
+        const val KEY_URL: String = "URL"
+    }
+
+    private lateinit var klarnaHybridSDK: KlarnaHybridSDK
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        setupSDK()
+        setupAddressBar()
+        intent.getStringExtra(KEY_URL)?.let {
+            loadUrl(it)
+        }
+    }
+
+    private fun setupSDK() {
+        klarnaHybridSDK = KlarnaHybridSDK("${getString(R.string.return_url_scheme)}://${getString(R.string.return_url_host)}", this)
+        webView.webViewClient = MyWebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        klarnaHybridSDK.addWebView(webView)
+    }
+
+    private fun setupAddressBar() {
+        imageViewLoadAddress.setOnClickListener {
+            loadUrl(editTextAddressBar.text.toString().trim())
+        }
+        editTextAddressBar.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    imageViewLoadAddress.performClick()
+                    return true
+                }
+                return false
+            }
+        })
+    }
+
+    private fun loadUrl(url: String) {
+        var address = url
+        if (!address.contains("://")) {
+            address = "https://$address"
+        }
+        webView.loadUrl(address)
+        editTextAddressBar.setText(address)
+        editTextAddressBar.setSelection(address.length)
+
+        // Dismiss the keyboard
+        editTextAddressBar.clearFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(editTextAddressBar?.windowToken, 0)
+    }
+
+
+    override fun didHideFullscreenContent(webView: WebView, completion: OnCompletion) {
+        showMessage("didHideFullscreenContent")
+        completion.run()
+    }
+
+    override fun didShowFullscreenContent(webView: WebView, completion: OnCompletion) {
+        showMessage("didShowFullscreenContent")
+        completion.run()
+    }
+
+    override fun onErrorOccurred(webView: WebView, error: KlarnaMobileSDKError) {
+        showMessage("onErrorOccurred: " + error.message)
+    }
+
+    override fun willHideFullscreenContent(webView: WebView, completion: OnCompletion) {
+        showMessage("willHideFullscreenContent")
+        completion.run()
+    }
+
+    override fun willShowFullscreenContent(webView: WebView, completion: OnCompletion) {
+        showMessage("willShowFullscreenContent")
+        completion.run()
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        Log.d(javaClass.simpleName, message)
+    }
+
+    inner class MyWebViewClient :
+        WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            return !klarnaHybridSDK.shouldFollowNavigation(url)
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            klarnaHybridSDK.newPageLoad(view)
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            editTextAddressBar.setText(url)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+            return
+        }
+        super.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
+    }
+}
